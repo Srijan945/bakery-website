66d9f29e862c2ee61a2f532a4ed69324692fa337
Srijan945 <sainisrijan95820@gmail.com>
2023-07-09 18:12:46 +0530

    Add hashrouter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,18 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom';
 import Home from './Home';
 import Cart from './Cart';
 import ErrorPage from './ErrorPage';
 import Payments from './Payments';
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' exact Component={Home} />
         <Route path='/cart' exact Component={Cart} />
         <Route path='/payments' exact Component={Payments} />
         <Route path='*' Component={ErrorPage} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
